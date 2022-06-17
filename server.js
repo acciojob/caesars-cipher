91c46fc83da47001cc432f4b4db6b8294242ee58
Mithilesh Tiwari <shubhamtiwari.tiwari84@gmail.com>
2022-06-18 00:45:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
-const app = require("./index.js");
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const answer = require("./index.js");
+
 app.listen(3000, () => {
   console.log('server started');
+});
+
+app.get('/', (req, res) => {
+  res.send(answer("SERR YBIR? NPPVBWBO"))
 });
